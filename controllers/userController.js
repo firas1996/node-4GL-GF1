9a7897486da488f7a4ad00558457e0dea5a9d93a
firas hamila <firas.hm.1996@gmail.com>
2024-10-29 11:38:05 +0100

    aaaa

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,3 +15,36 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      message: "Users Fetched !!!",
+      data: { users },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      // results: users.length,
+      message: "Users Fetched !!!",
+      data: { user },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 
-router.route("/").post(userController.createUser);
+router
+  .route("/")
+  .post(userController.createUser)
+  .get(userController.getAllUsers);
+router.route("/:id").get(userController.getUserById);
 
 module.exports = router;
